757f743cec8ddd87f24f07cc9f6a6c53fcafd3f7
eelanagaraj <7308464+eelanagaraj@users.noreply.github.com>
2020-12-23 10:29:06 +0100

    Refactor and move blockTransaction logic to block

--- services/block_service.go
+++ services/block_service.go
@@ -25,6 +25,7 @@ import (
 	"github.com/coinbase/rosetta-sdk-go/client"
 	"github.com/coinbase/rosetta-sdk-go/types"
 	"github.com/ethereum/go-ethereum/common"
+	gethTypes "github.com/ethereum/go-ethereum/core/types"
 )
 
 // Implements the server.BlockAPIServicer interface.
@@ -40,22 +41,98 @@ func NewBlockAPIService(
 	}
 }
 
+// Extract operations from transferLog
+// and update opIndex, operations, prevRelatedOps in place accordingly.
+func opsFromLog(
+	transferLog gethTypes.Log,
+	opIndex *int64,
+	operations *[]*types.Operation,
+	relatedOps *[]*types.OperationIdentifier,
+) {
+	// TODO ?: Is there a more direct/better way of converting Topic Hash -> Address?
+	from := common.HexToAddress(transferLog.Topics[1].Hex())
+	to := common.HexToAddress(transferLog.Topics[2].Hex())
+
+	value := new(big.Int).SetBytes(transferLog.Data)
+	// TODO ?: Can failed transfers appear in the logs with status !Removed?
+	var status types.OperationStatus
+	if transferLog.Removed {
+		status = OpFailed
+	} else {
+		status = OpSuccess
+	}
+
+	var opType string
+	var inGroup bool = false
+	// Distinguish between balance-changing transactions which emit "Transfer" event
+	switch {
+	case to == ZeroAddress:
+		if from == ZeroAddress {
+			return
+		}
+		opType = OpBurn
+		// Reset related ops, treat burns as standalone
+		relatedOps = &[]*types.OperationIdentifier{}
+	case from == ZeroAddress:
+		opType = OpMint
+		// Reset related ops, treat mints as standalone
+		relatedOps = &[]*types.OperationIdentifier{}
+	default:
+		// TODO: for now, cannot differentiate between transfers and gas fees
+		opType = OpTransfer
+		inGroup = true
+	}
+	processOp := func(address common.Address, opValue *big.Int, inGroup bool) {
+		op := newAtomicOp(address, *opIndex, opValue, status, opType, *relatedOps)
+		*operations = append(*operations, op)
+		*opIndex++
+		// Do not include standalone ops in a related group
+		if inGroup {
+			*relatedOps = append(op.RelatedOperations, op.OperationIdentifier)
+		}
+		return
+	}
+	// Split operations into atomic ops with effects per-account
+	if from != ZeroAddress {
+		processOp(from, new(big.Int).Neg(value), inGroup)
+	}
+	if to != ZeroAddress {
+		processOp(to, value, inGroup)
+	}
+	return
+}
+
+func callParamsFromBlock(
+	block int64,
+	networkId *types.NetworkIdentifier,
+) (*types.CallRequest, error) {
+	blockIdStr := strconv.FormatInt(block, 10)
+	rawParams := &CallLogsParams{
+		Event:     "StableToken.Transfer",
+		FromBlock: blockIdStr, // fetch single block
+		ToBlock:   blockIdStr,
+	}
+	paramsMap, err := airgap.MarshallToMap(rawParams)
+	if err != nil {
+		return nil, err
+	}
+	return &types.CallRequest{
+		NetworkIdentifier: networkId,
+		Method:            "celo_getLogs",
+		Parameters:        paramsMap,
+	}, nil
+}
+
 // endpoint: /block
 func (s *BlockAPIService) Block(
 	ctx context.Context,
 	request *types.BlockRequest,
 ) (*types.BlockResponse, *types.Error) {
 
-	// TODO revisit if we need to filter out non-cUSD transactions; currently return all transactions
 	blockResp, clientErr, _ := s.client.BlockAPI.Block(ctx, request)
-	// return blockResp, clientErr
 	if clientErr != nil {
 		return nil, clientErr
 	}
-
-	// grab logs for block
-	// parse transactions per log
-	// ignore other_transactions? -- or take set difference of these transactions at the end
 
 	// Prior to threshold, StableCoin contract not registered on chain and cannot be accessed via /call
 	var threshold int64
@@ -68,37 +145,22 @@ func (s *BlockAPIService) Block(
 		log.Printf("Unknown StableCoin registration for Network %s\n", request.NetworkIdentifier.Network)
 		return nil, ErrValidation
 	}
-	
+
 	if blockResp.Block.BlockIdentifier.Index < threshold {
 		// TODO think about other_transactions
 		blockResp.Block.Transactions = nil
 		return blockResp, nil
 	}
 
-	// get the filtered logs for transfer events in the requested block
-
-	// TODO: func callLogsForBlock() --> get callLogs for this block
-	blockIdStr := strconv.FormatInt(blockResp.Block.BlockIdentifier.Index, 10)
-	rawParams := &CallLogsParams{
-		Event:     "StableToken.Transfer",
-		FromBlock: blockIdStr, // fetch single block
-		ToBlock:   blockIdStr,
-	}
-	paramsMap, err := airgap.MarshallToMap(rawParams)
+	// Get the filtered logs for transfer events in the requested block
+	callReq, err := callParamsFromBlock(blockResp.Block.BlockIdentifier.Index, request.NetworkIdentifier)
 	if err != nil {
 		return nil, ErrValidation
 	}
-	callReq := &types.CallRequest{
-		NetworkIdentifier: request.NetworkIdentifier,
-		Method:            "celo_getLogs",
-		Parameters:        paramsMap,
-	}
-
 	resp, _, err := s.client.CallAPI.Call(ctx, callReq)
 	if err != nil {
 		return nil, ErrCeloClient
 	}
-
 	var result rpc.CallLogsResult
 	err = airgap.UnmarshallFromMap(resp.Result, &result)
 	if err != nil {
@@ -106,95 +168,44 @@ func (s *BlockAPIService) Block(
 	}
 
 	transactions := []*types.Transaction{}
-	var opIndex int64
-	var currTxHash common.Hash // TODO --> tx identifier instead?
-	var operations []*types.Operation
-	var prevRelatedOps *[]*types.OperationIdentifier
+	var opIndex *int64 = new(int64)
+	var currTxHash common.Hash
+	var operations *[]*types.Operation
+	var relatedOps *[]*types.OperationIdentifier
 
 	for i, transferLog := range result.Logs {
-
 		if transferLog.TxHash != currTxHash {
 			// Append fully parsed currTxHash + operations to list of transactions
 			if i != 0 {
 				transaction := &types.Transaction{
 					TransactionIdentifier: &types.TransactionIdentifier{Hash: currTxHash.String()},
-					Operations: operations,
+					Operations:            *operations,
 				}
-				transactions = append(transactions, transaction)	
+				transactions = append(transactions, transaction)
 			}
 			// Set currTxHash (to current hash) and initialize start of transaction
 			currTxHash = transferLog.TxHash
-			operations = []*types.Operation{}
-			prevRelatedOps = &[]*types.OperationIdentifier{}
-			opIndex = 0
+			operations = &[]*types.Operation{}
+			relatedOps = &[]*types.OperationIdentifier{}
+			*opIndex = 0
 		}
+		// Update the index, operations, relatedOps in place
+		opsFromLog(transferLog, opIndex, operations, relatedOps)
 
-		// TODO ?: Is there a more direct/better way of converting Topic Hash -> Address?
-		from := common.HexToAddress(transferLog.Topics[1].Hex())
-		to := common.HexToAddress(transferLog.Topics[2].Hex())
-
-		value := new(big.Int).SetBytes(transferLog.Data)
-		// TODO ?: Can failed transfers appear in the logs with status !Removed?
-		var status types.OperationStatus
-		if transferLog.Removed {
-			status = OpFailed
-		} else {
-			status = OpSuccess
-		}
-
-		var opType string
-		var inGroup bool = false
-		// Distinguish between balance-changing transactions which emit "Transfer" event
-		switch {
-		case to == ZeroAddress:
-			if from == ZeroAddress {
-				continue
-			}
-			opType = OpBurn
-			// Reset related ops, treat burns as standalone
-			prevRelatedOps = &[]*types.OperationIdentifier{}
-		case from == ZeroAddress:
-			opType = OpMint
-			// Reset related ops, treat mints as standalone
-			prevRelatedOps = &[]*types.OperationIdentifier{}
-		default:
-			// TODO: for now, cannot differentiate between transfers and gas fees
-			opType = OpTransfer
-			inGroup = true
-		}
-
-		processOp := func(address common.Address, opValue *big.Int, inGroup bool) {
-			op := newAtomicOp(address, opIndex, opValue, status, opType, *prevRelatedOps)
-			operations = append(operations, op)
-			opIndex += 1
-			// Do not include standalone ops in a related group
-			if inGroup {
-				*prevRelatedOps = append(op.RelatedOperations, op.OperationIdentifier)
-			}
-		}
-		// Split operations into atomic ops with effects per-account
-		if from != ZeroAddress {
-			processOp(from, new(big.Int).Neg(value), inGroup)
-		}
-		if to != ZeroAddress {
-			processOp(to, value, inGroup)
-		}
-		if i == len(result.Logs) - 1 {
-			// Ensure last seen log is included in transaction list
+		// Ensure last seen log is included in transaction list
+		if i == len(result.Logs)-1 {
 			transaction := &types.Transaction{
 				TransactionIdentifier: &types.TransactionIdentifier{Hash: currTxHash.String()},
-				Operations: operations,
+				Operations:            *operations,
 			}
 			transactions = append(transactions, transaction)
 		}
-
 	}
-	// TODO append the last processed transaction in the list (or at the end of for loop --> if i == len(result.logs) - 1)
+
 	blockResp.Block.Transactions = transactions
 	blockResp.OtherTransactions = nil
 
 	return blockResp, nil
-
 }
 
 func newAtomicOp(
@@ -223,129 +234,28 @@ func (s *BlockAPIService) BlockTransaction(
 	ctx context.Context,
 	request *types.BlockTransactionRequest,
 ) (*types.BlockTransactionResponse, *types.Error) {
-
-	// Prior to threshold, StableCoin contract not registered on chain and cannot be accessed via /call
-	var threshold int64
-	switch networkId := request.NetworkIdentifier.Network; networkId {
-	case MainnetId:
-		threshold = StableCoinRegisteredMainnet
-	case TestnetId:
-		threshold = StableCoinRegisteredTestnet
-	default:
-		log.Printf("Unknown StableCoin registration for Network %s\n", request.NetworkIdentifier.Network)
-		return nil, ErrValidation
-	}
-
-	if request.BlockIdentifier.Index < threshold {
-		return &types.BlockTransactionResponse{
-			Transaction: &types.Transaction{
-				TransactionIdentifier: request.TransactionIdentifier,
-				Operations:            []*types.Operation{},
-			},
-		}, nil
-	}
-
-	// TODO moving logic to block (to do the looping in one pass as opposed to per transaction); this is a first pass
-	// ? for CB: should this all instead be happening in /block? i.e. computing all transactions + gas, etc.
-
-	// get the filtered logs for transfer events
-	blockIdStr := strconv.FormatInt(request.BlockIdentifier.Index, 10)
-	rawParams := &CallLogsParams{
-		Event:     "StableToken.Transfer",
-		FromBlock: blockIdStr, // fetch single block
-		ToBlock:   blockIdStr,
-	}
-	paramsMap, err := airgap.MarshallToMap(rawParams)
-	if err != nil {
-		return nil, ErrValidation
-	}
-	callReq := &types.CallRequest{
+	// ?: Is this endpoint even necessary if block contains required logic?
+	blockResp, clientErr := s.Block(ctx, &types.BlockRequest{
 		NetworkIdentifier: request.NetworkIdentifier,
-		Method:            "celo_getLogs",
-		Parameters:        paramsMap,
+		BlockIdentifier: &types.PartialBlockIdentifier{
+			Index: &request.BlockIdentifier.Index,
+			Hash:  &request.BlockIdentifier.Hash,
+		},
+	},
+	)
+	if clientErr != nil {
+		return nil, clientErr
 	}
-
-	resp, _, err := s.client.CallAPI.Call(ctx, callReq)
-	if err != nil {
-		return nil, ErrCeloClient
-	}
-
-	var result rpc.CallLogsResult
-	err = airgap.UnmarshallFromMap(resp.Result, &result)
-	if err != nil {
-		return nil, ErrValidation
-	}
-
-	var opIndex int64 = 0
-	
-	// TODO test
-	// var operations []*types.Operation
-	operations := []*types.Operation{}
-	prevRelatedOps := &[]*types.OperationIdentifier{}
-
-	// TODO: first pass -- more efficient to have this within block logic, but for now, match structure of core rosetta
-	// loop through the logs until the transaction matches the requested transaction hash
-	for _, transferLog := range result.Logs {
-		if transferLog.TxHash.String() != request.TransactionIdentifier.Hash {
-			continue
-		}
-
-		// TODO ?: Is there a more direct/better way of converting Topic Hash -> Address?
-		from := common.HexToAddress(transferLog.Topics[1].Hex())
-		to := common.HexToAddress(transferLog.Topics[2].Hex())
-
-		value := new(big.Int).SetBytes(transferLog.Data)
-		// TODO ?: Can failed transfers appear in the logs with status !Removed?
-		var status types.OperationStatus
-		if transferLog.Removed {
-			status = OpFailed
-		} else {
-			status = OpSuccess
-		}
-
-		var opType string
-		var inGroup bool = false
-		// Distinguish between balance-changing transactions which emit "Transfer" event
-		switch {
-		case to == ZeroAddress:
-			if from == ZeroAddress {
-				continue
-			}
-			opType = OpBurn
-			// Reset related ops, treat burns as standalone
-			prevRelatedOps = &[]*types.OperationIdentifier{}
-		case from == ZeroAddress:
-			opType = OpMint
-			// Reset related ops, treat mints as standalone
-			prevRelatedOps = &[]*types.OperationIdentifier{}
-		default:
-			// TODO: for now, cannot differentiate between transfers and gas fees
-			opType = OpTransfer
-			inGroup = true
-		}
-
-		processOp := func(address common.Address, opValue *big.Int, inGroup bool) {
-			op := newAtomicOp(address, opIndex, opValue, status, opType, *prevRelatedOps)
-			operations = append(operations, op)
-			opIndex += 1
-			// Do not include standalone ops in a related group
-			if inGroup {
-				*prevRelatedOps = append(op.RelatedOperations, op.OperationIdentifier)
-			}
-		}
-		// Split operations into atomic ops with effects per-account
-		if from != ZeroAddress {
-			processOp(from, new(big.Int).Neg(value), inGroup)
-		}
-		if to != ZeroAddress {
-			processOp(to, value, inGroup)
+	// Find and return the specified transaction
+	for _, t := range blockResp.Block.Transactions {
+		if t.TransactionIdentifier.Hash == request.TransactionIdentifier.Hash {
+			return &types.BlockTransactionResponse{Transaction: t}, nil
 		}
 	}
-
+	// Transaction not found or does not contain cUSD relevant operations, return empty
 	return &types.BlockTransactionResponse{
 		Transaction: &types.Transaction{
 			TransactionIdentifier: request.TransactionIdentifier,
-			Operations:            operations,
 		},
 	}, nil
 }
